0e9a9f58768b99837147e589a10f2dbdce532977
Daniel Diaz <110594320+ddi4z@users.noreply.github.com>
2023-02-02 10:33:36 -0500

    documentacion

--- project0/cod.py
+++ project0/cod.py
@@ -1,6 +1,21 @@
 archivo= open("ejemplo.txt").read().lower()
 
 
+"""
+La función lexer toma como entrada un archivo de texto 
+y devuelve una lista con las palabras y símbolos encontrados 
+en el archivo.
+
+La función utiliza un ciclo while que itera sobre los caracteres
+en el archivo hasta que se han procesado todos los caracteres. 
+Para cada iteración, la función llama a generar_palabra para generar 
+la siguiente palabra y símbolo en el archivo. La palabra y símbolo 
+son agregados a la lista rta.
+
+Si el símbolo siguiente a la palabra generada no es un espacio en 
+blanco, entonces se agrega a la lista rta.
+"""
+
 def lexer (archivo):
     rta= list()
     j=0
@@ -11,6 +26,22 @@ def lexer (archivo):
             rta.append(siguientepalabra)
     rta[0]=rta[0].replace("\n","")
     return rta
+
+
+"""
+El objetivo de la función es generar una palabra del archivo 
+de texto, iterando a través de los caracteres en el archivo 
+desde la posición i hasta que se encuentra un símbolo de cierre.
+
+El conjunto de símbolos de cierre está definido como 
+{" ",",",",","[","]",":","|"} y se utiliza para detener la 
+iteración y retornar la palabra generada, el nuevo índice (i+1) y 
+el símbolo de cierre encontrado.
+
+La palabra generada es construida a partir de una lista 
+generador_palabra que se rellena con los caracteres del archivo, 
+y se convierte en una cadena de texto mediante el método join. 
+"""
 
 def generar_palabra (archivo, i):
     generador_palabra= []
